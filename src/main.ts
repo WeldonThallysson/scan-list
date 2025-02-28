ef170f78b2990edfe0e9852b49f4cceeaf76e00c
Weldon Thallysson <weldonthallysson.dev@gmail.com>
2025-02-28 00:04:41 -0300

    integrações

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express'
+import {VercelRequest, VercelResponse} from '@vercel/node'
+import * as dotenv from 'dotenv'
 
-dotenv.config();
-const server = express();
-
+dotenv.config()
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  await app.init();
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors()
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
 
-export default async function handler(req: VercelRequest, res:VercelResponse) {
+
+export default async function handler(req: VercelRequest, res: VercelResponse){
   const app = await NestFactory.create(AppModule);
   app.enableCors()
+  await app.init()
 
-  await app.init();
-  app.getHttpAdapter().getInstance()(req, res);
-}
+  app.getHttpAdapter().getInstance()(req,res)}
