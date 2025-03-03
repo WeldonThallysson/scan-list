9ef286747fba9aed30469a7ccdd298c5c514be2c
Weldon Thallysson <weldonthallysson.dev@gmail.com>
2025-03-02 21:34:28 -0300

    integrações

--- dist/src/main.js
+++ dist/src/main.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = handler;
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const dotenv = require("dotenv");
@@ -13,4 +14,10 @@ async function bootstrap() {
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+async function handler(req, res) {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors();
+    await app.init();
+    app.getHttpAdapter().getInstance()(req, res);
+}
 //# sourceMappingURL=main.js.map
