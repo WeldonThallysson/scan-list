ec8ff1c3ca56bc993a7f0594e73ad100ecd50c89
Weldon Thallysson <weldonthallysson.dev@gmail.com>
2025-03-02 21:49:35 -0300

    integrações

--- dist/src/main.js
+++ dist/src/main.js
@@ -3,19 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = handler;
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const dotenv = require("dotenv");
-dotenv.config();
 let cachedApp = null;
 async function createNestApp() {
-    if (!cachedApp) {
-        const app = await core_1.NestFactory.create(app_module_1.AppModule);
-        app.enableCors({
-            methods: 'GET,POST,PUT,DELETE',
-            allowedHeaders: 'Content-Type, Authorization',
-        });
-        await app.init();
-        cachedApp = app;
-    }
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors();
+    await app.init();
     return cachedApp;
 }
 async function handler(req, res) {
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 let cachedApp: any = null;
-
 async function createNestApp() {
-  // Só cria a aplicação se ainda não tiver sido criada
-  if (!cachedApp) {
     const app = await NestFactory.create(AppModule);
-    app.enableCors({
-      methods: 'GET,POST,PUT,DELETE',
-      allowedHeaders: 'Content-Type, Authorization',
-    });
+    
+    app.enableCors();
     await app.init();
-    cachedApp = app; // Armazena a instância criada
-  }
+ 
   return cachedApp;
 }
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const app = await createNestApp();
 
-  // Processa a requisição com a instância do NestJS já criada
   app.getHttpAdapter().getInstance()(req, res);
 }
